20aae708d35e9941f46ec45fb91e8db980926250
sanket bodke <sanketbodke26@gmail.com>
2023-06-01 00:11:10 +0530

    modal add

---
+++ modal/script.js
@@ -0,0 +1,7 @@
+var modal = document.querySelector('.modal_content');
+var close = document.querySelector('.close_btn');
+var button = document.querySelector('.btn');
+
+// button.style.color = "red";
+
+// function open_modal() {}
